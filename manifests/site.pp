20c47521a3a10c403f49b340243bdc1b94a81c7b
George Bashi <jamil@georgebashi.com>
2013-06-01 12:36:17 +0100

    blah ruby blah

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,7 +15,16 @@ exec { "apt-get update":
 
 Exec["apt-get update"] -> Package <| |>
 
-package { ["git", "rbenv", "openjdk-7-jre", "build-essential", "lxc-docker", "linux-image-generic-lts-raring"]:
+package { [
+    "git",
+    "rbenv",
+    "openjdk-7-jre",
+    "build-essential",
+    "lxc-docker",
+    "linux-image-generic-lts-raring",
+    "linux-headers-3.8.0-22-generic",
+    "dkms",
+    ]:
     ensure => installed,
 }
 
@@ -25,22 +34,38 @@ exec { "clone ruby-build":
     require => Package["git"]
 }
 
+file { "/home/vagrant/.rbenv":
+    ensure => directory,
+    owner  => vagrant,
+    group  => vagrant,
+}
+
 exec { "install jruby":
     command => "/home/vagrant/ruby-build/bin/ruby-build jruby-1.7.3 /home/vagrant/.rbenv/versions/jruby-1.7.3",
     creates => "/home/vagrant/.rbenv/versions/jruby-1.7.3",
-    require => [Package["rbenv", "openjdk-7-jre", "build-essential"], Exec["clone ruby-build"]],
+    user    => vagrant,
+    require => [Package["rbenv", "openjdk-7-jre", "build-essential"], Exec["clone ruby-build"], File["/home/vagrant/.rbenv"]],
 }
 
-exec { "/usr/bin/rbenv global jruby-1.7.3":
+file { "/home/vagrant/.rbenv/global":
+    content => "jruby-1.7.3",
     require => Exec["install jruby"],
 }
 
 file { "/etc/profile.d/rbenv.sh":
-    content => "export PATH=\"/home/vagrant/.rbenv/shims:\${PATH}\""
+    content => "export PATH=\"/home/vagrant/.rbenv/shims:\${PATH}\"",
+    require => Exec["install jruby"],
 }
 
-exec { "install jpi":
-    command => "/home/vagrant/.rbenv/shims/gem install jpi",
+exec { "/usr/bin/rbenv rehash":
+    require => File["/home/vagrant/.rbenv/global"],
+    user    => vagrant,
+}
+
+exec { "install gems":
+    command => "/home/vagrant/.rbenv/shims/gem install bundler",
     onlyif  => "/usr/bin/test ! -d /home/vagrant/.rbenv/versions/jruby-1.7.3/lib/ruby/gems/shared/gems/jpi-*/",
+    user    => vagrant,
+    require => [File["/home/vagrant/.rbenv/global"], Exec["/usr/bin/rbenv rehash"]],
 }
 
